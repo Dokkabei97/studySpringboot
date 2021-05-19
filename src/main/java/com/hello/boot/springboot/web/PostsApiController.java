a380d45c39273c8f72caea64e134a7c21d58836a
Dokkabei97 <wkdrn970@naver.com>
2021-05-19 21:57:43 +0900

    대가리 터지것다

---
+++ src/main/java/com/hello/boot/springboot/web/PostsApiController.java
@@ -0,0 +1,21 @@
+package com.hello.boot.springboot.web;
+
+import com.hello.boot.springboot.service.posts.PostsService;
+import com.hello.boot.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postsService.save(requestDto);
+    }
+}
