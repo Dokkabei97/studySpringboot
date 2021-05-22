dd762767b86815f2f63c4df379e5d8eb97166dbb
Dokkabei97 <wkdrn970@naver.com>
2021-05-22 19:39:39 +0900

    글 수정

--- src/main/java/com/hello/boot/springboot/service/posts/PostsService.java
+++ src/main/java/com/hello/boot/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.hello.boot.springboot.service.posts;
 
 import com.hello.boot.springboot.domain.posts.Posts;
 import com.hello.boot.springboot.domain.posts.PostsRepository;
+import com.hello.boot.springboot.web.dto.PostsListResponseDto;
 import com.hello.boot.springboot.web.dto.PostsResponseDto;
 import com.hello.boot.springboot.web.dto.PostsSaveRequestDto;
 import com.hello.boot.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -35,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 조회 기능만 남겨두어 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // == map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
