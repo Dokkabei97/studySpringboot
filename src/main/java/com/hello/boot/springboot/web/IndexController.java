9723853b2f9a1a742018afba82f4cbc72b1af19e
Dokkabei97 <wkdrn970@naver.com>
2021-05-23 02:21:49 +0900

    스프링 시큐리티 + 구글 로그인

--- src/main/java/com/hello/boot/springboot/web/IndexController.java
+++ src/main/java/com/hello/boot/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hello.boot.springboot.web;
 
+import com.hello.boot.springboot.config.auth.dto.SessionUser;
 import com.hello.boot.springboot.service.posts.PostsService;
 import com.hello.boot.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
+        if (user != null) { // 세션에 저장돈 값이 있을 때만 model에 userName으로 등록
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
