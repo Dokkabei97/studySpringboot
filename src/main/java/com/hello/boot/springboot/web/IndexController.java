fc82ad5687e92fb4ada33ab95609f1b111e2cd01
Dokkabei97 <wkdrn970@naver.com>
2021-05-25 22:54:38 +0900

    어노테이션 + 개선

--- src/main/java/com/hello/boot/springboot/web/IndexController.java
+++ src/main/java/com/hello/boot/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.hello.boot.springboot.web;
 
+import com.hello.boot.springboot.config.auth.LoginUser;
 import com.hello.boot.springboot.config.auth.dto.SessionUser;
 import com.hello.boot.springboot.service.posts.PostsService;
 import com.hello.boot.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         // CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장
         if (user != null) { // 세션에 저장돈 값이 있을 때만 model에 userName으로 등록
             model.addAttribute("userName", user.getName());
