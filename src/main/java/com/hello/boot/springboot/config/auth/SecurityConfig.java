82f88b59d210290cdaec6aa626a77a250397844d
Dokkabei97 <wkdrn970@naver.com>
2021-06-01 13:01:55 +0900

    nginx + 테스트코드 추가

--- src/main/java/com/hello/boot/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/hello/boot/springboot/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션들을 disable 한다
                 .and()
                     .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점 authorizeRequests가 선언되면 antMatchers 옵션을 사용할 수 있다
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll() // 권한 관리 대상을 지정하는 옵션 URL, HTTP메소드별로 관리 가능
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll() // 권한 관리 대상을 지정하는 옵션 URL, HTTP메소드별로 관리 가능
                     // "/" 등 지정된 URL들은 permitAll()옵션을 통해 전체 열람 권한 제공
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // "/api/v1/**" 주소를 가진 API는 USER권한을 가진 사람만 가능하게 설정
                     .anyRequest().authenticated() // 설정된 값들 이외 나머지 URL을 나타낸다
